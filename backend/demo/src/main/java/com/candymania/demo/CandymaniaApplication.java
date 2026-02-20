5b4c536e7b5d7eaa3900d00c21e8ae913e17989e
Paulo <paulog162003@gmail.com>
2026-02-20 10:44:45 -0300

    primer commit

---
+++ backend/demo/src/main/java/com/candymania/demo/CandymaniaApplication.java
@@ -0,0 +1,13 @@
+package com.candymania.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandymaniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandymaniaApplication.class, args);
+	}
+
+}
---
+++ backend/demo/src/test/java/com/candymania/demo/CandymaniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.candymania.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandymaniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
